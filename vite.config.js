9c1f5c9271561ba21c5584f104641801c050a4e0
diipperss <dibya6665@gmail.com>
2024-08-06 20:10:21 +0800

    changes to vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/website-portfolio/', // replace with your repository name
   plugins: [react()],
-  assetsInclude:['**/*.glb']
+  assetsInclude: ['**/*.glb']
 })
